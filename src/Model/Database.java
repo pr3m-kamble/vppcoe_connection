90e1d2c7516427580aa8336e0faef63c4dd2bb0f
Prem Kamble <premkamble099@gmail.com>
2025-02-03 14:58:01 +0530

    Add files via upload

---
+++ src/Model/Database.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import View.Alert;
+
+public class Database {
+	
+	private String user = "root";
+	private String pass = "1234";
+	private String url = "jdbc:mysql://localhost/socialmedia";
+	private Statement statement;
+	
+	public Database() {
+		try {
+			Connection connection = DriverManager.getConnection(url, user, pass);
+			statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+									ResultSet.CONCUR_READ_ONLY);
+		} catch (SQLException e) {
+			new Alert(e.getMessage(), null);
+		}
+	}
+	
+	public Statement getStatement() {
+		return statement;
+	}
+
+}
